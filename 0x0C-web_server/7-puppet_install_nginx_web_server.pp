953c0c3258c155f0cfa2163dbc267c3b72c5ddcc
iLouis1 <rhiganakatugba@gmail.com>
2024-03-26 15:42:15 -0700

    Done

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Configure server using puppet
+
+# defines a Puppet class called nginx_server that
+
+package { 'nginx':
+  ensure     => 'installed',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
